2bdf45bb89e87a8754cae9533b0f6ec69e67ec1b
shotaro nakajima <nakajimathirddisp@gmail.com>
2020-11-29 00:03:56 +0900

    add: 一旦完成

---
+++ src/extension.ts
@@ -0,0 +1,102 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import { window, StatusBarItem, StatusBarAlignment, Position, Range } from 'vscode';
+import * as vscode from 'vscode';
+import * as copyPaste from 'copy-paste';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "encode-and-paste" is now active!');
+
+	let paster = new Paster();
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('encode-and-paste.paste', () => {
+		// The code you place here will be executed every time your command is executed
+        paster.paste();
+		// Display a message box to the user
+        vscode.window.showInformationMessage('pasted encorded text!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+
+export class Paster {
+    private _statusBarItem: vscode.StatusBarItem;
+
+    public paste() {
+        if (!this._statusBarItem) {
+            this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+        }
+
+        copyPaste.paste((error, content) => {
+            if (content) {
+                this.generateEncodedText(content);
+            } else {
+                this.showMessage();
+            }
+        });
+    }
+
+    generateEncodedText(content) {
+        var _this = this;
+        var formattedContent = this.unicodeEscape(content);
+        var prefix = vscode.workspace.getConfiguration('encodeAndPaste').get('prefix');
+
+        if (typeof prefix === 'string') {
+            const prefixString = prefix.replace(/\\/gi, "\\\\");
+            formattedContent = formattedContent.replace(/\\/gi, prefixString);
+        }
+
+        _this.writeToEditor(formattedContent);
+    }
+
+    unicodeEscape(str) {
+        if (!String.prototype.repeat) {
+            String.prototype.repeat = function(digit) {
+                var result = '';
+                for (var i = 0; i < Number(digit); i++) result += str;
+                return result;
+            };
+        }
+
+        var strs = str.split(''), hex, result = '';
+
+        for (var i = 0, len = strs.length; i < len; i++) {
+            hex = strs[i].charCodeAt(0).toString(16);
+            result += '\\u' + ('0'.repeat(Math.abs(hex.length - 4))) + hex;
+        }
+        return result;
+    };
+
+    writeToEditor(content) {
+        const editor = vscode.window.activeTextEditor;
+
+        if (typeof editor !== 'undefined') {
+            let startLine = editor.selection.start.line;
+            var selection = editor.selection;
+            let position = new vscode.Position(startLine, selection.start.character);
+            return editor.edit((editBuilder) => {
+                editBuilder.insert(position, content);
+            });
+        }
+    }
+
+    showMessage(content=null) {
+        this._statusBarItem.text = "Paste encoded text!";
+        this._statusBarItem.show();
+        setTimeout(() => {
+            this._statusBarItem.hide();
+        }, 3000);
+    }
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
